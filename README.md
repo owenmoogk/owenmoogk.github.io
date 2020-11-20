99af5e8a1e8b7dbb997394bf025a93f574c403ce
owenmoogk <owenmoogk@gmail.com>
2020-11-20 16:54:07 -0500

    added files

---
+++ README.md
@@ -0,0 +1,5 @@
+Website Version 3
+
+Work in progress, wanted an upgrade from my older website.
+
+Up on github pages, but please still use the website at owenmoogk.codes, as it contains more information
---
+++ js/navbar.js
@@ -0,0 +1,25 @@
+function loadMenuItems() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			menuItems(this);
+		}
+    };
+    xmlhttp.open("GET", "/assets/menu-items.xml", true);
+    xmlhttp.send();
+}
+function menuItems(xml) {
+    var titles, links, i, txt, xmlDoc; 
+    xmlDoc = xml.responseXML;
+    txt = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+    titles = xmlDoc.getElementsByTagName("title");
+    links = xmlDoc.getElementsByTagName('link');
+    for (i = 0; i < titles.length; i++) { 
+        txt += '<a href = '+links[i].childNodes[0].nodeValue+' class = "navlinks">'+titles[i].childNodes[0].nodeValue+'</a> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp';
+    }
+    document.getElementById("navBox").innerHTML = txt;
+    var image = '/img/index/background.jpg'
+    txt = document.getElementById('title').innerHTML
+    txt += "<div class = 'nav-img-container'><img class = 'nav-img' src ="+image+"></div>"
+    document.getElementById('title').innerHTML = txt
+}
