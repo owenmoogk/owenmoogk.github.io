3b85d22cbf736b4dc22248fcee8f64d38c0a855c
owenmoogk <owenmoogk@gmail.com>
2020-11-21 12:15:34 -0500

    reverted :8ball:

--- js/js-for-all.js
+++
@@ -1,4 +0,0 @@
-function loadHead(){
-    $('head').append('<meta name="author" content="Owen Moogk"><meta name="keywords" content="Owen, Moogk, Owenmoogk"></meta><meta name="description" content="Just a little bit about me :)">')
-    $('head').append('<title>Owen Moogk - Blog</title>');
-}
