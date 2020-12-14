76ba1837d1fc4d6433d0a0937bfd1f4c080bb89a
owenmoogk <owenmoogk@gmail.com>
2020-12-13 20:05:06 -0500

    fixed the mobile nav

--- js/navbar.js
+++ js/navbar.js
@@ -13,7 +13,6 @@ function menuItems(xml) {
     
     // text that is appended to elements
     var txt = ''
-    var modalTxt = ''
 
     // xml variables
     xmlDoc = xml.responseXML;
@@ -23,39 +22,61 @@ function menuItems(xml) {
     // loading text variables
     for (i = 0; i < titles.length; i++) { 
         txt += '<a href = '+links[i].childNodes[0].nodeValue+' class = "navlinks">'+titles[i].childNodes[0].nodeValue+'</a>';
-        modalTxt += '<a href = '+links[i].childNodes[0].nodeValue+' class = "navlinks">'+titles[i].childNodes[0].nodeValue+'</a>';
     }
-    modalTxt += '<p class="close" onClick="modalClose()">&times</p>'
-    // text with modal contains the text with the modal html. this is so we can also import text into the modal
-    txt = txt+'<div id="nav-button"><img class="nav-img" src="/img/open-nav-button.png"></div>'
-    txt += '<div id="myModal" class="modal"><div class="modal-content"><p class="modal-text">Some text in the Modal..</p></div></div>'
+    txt = txt+'<div id="nav-button" onclick="modalOpen()"><img class="nav-img" src="/img/open-nav-button.png"></div>'
+    
+    // modal txt
+    txt += '<div id="myModal" class="modal"><div class="modal-content">'
+    for (i = 0; i < titles.length; i++) { 
+        txt += '<a href = '+links[i].childNodes[0].nodeValue+' class = "modalLinks">'+titles[i].childNodes[0].nodeValue+'</a>';
+    }
+    txt += '<span class="close" onclick="modalClose()">&times;</span></div></div>'
 
     // side nav
     document.getElementById('nav').innerHTML = txt
-
-    // modal text
-    var modal = document.getElementById("myModal");
-    var modalText = document.getElementsByClassName("modal-text")[0];
-    var button = document.getElementById("nav-button");
-    modalText.innerHTML = modalTxt
-    button.onclick = function() {
-        modalOpen()
-    }
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modalClose()
-        }
-    }
 }
 function modalClose(){
     var modal = document.getElementById("myModal");
-    var body = document.getElementsByTagName('body')[0];
-    modal.style.display = "none";
-    body.classList.remove("overlay");
+    var body = document.getElementsByClassName('body');
+    var element;
+    if (body.length == 1){
+        element = body[0]
+    }
+    else{
+        body = document.getElementsByClassName('main'); // my stupid naming ¯\_(ツ)_/¯
+        if (body.length == 1){
+            element = body[0]
+        }
+        else{
+            console.log('houston we have an error') // should never happen
+        }
+    }
+	modal.style.display = 'none'
+    element.style.filter = "blur(0px)"
 }
 function modalOpen(){
     var modal = document.getElementById("myModal");
-    var body = document.getElementsByTagName('body')[0];
-    modal.style.display = "flex";
-    body.classList.add("overlay");
+    
+    // getting the body, putting it in the element variable
+    var body = document.getElementsByClassName('body');
+    var element;
+    if (body.length == 1){
+        element = body[0]
+    }
+    else{
+        body = document.getElementsByClassName('main'); // my stupid naming ¯\_(ツ)_/¯
+        if (body.length == 1){
+            element = body[0]
+        }
+        else{
+            console.log('houston we have an error') // should never happen
+        }
+    }
+    element.style.filter = "blur(15px)"
+    $(modal).slideDown("slow");
+}
+window.onclick = function(event) {
+    if (event.target == document.getElementById("myModal")) {
+      	modalClose()
+    }
 }
