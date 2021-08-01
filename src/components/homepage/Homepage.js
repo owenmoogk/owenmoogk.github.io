94ea09d83a5c270737b7b5e20b7ca50c87bee10f
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-08-01 17:06:00 -0400

    change homepage look :eyes:

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,55 +1,12 @@
-import React, {useState, useEffect} from 'react'
-
 export default function Homepage() {
-
-	function loadSplash(){
-		fetch(process.env.PUBLIC_URL+'/splashes.txt')
-		.then(response => response.text())
-		.then(data => {
-			let items = data.split(/\r?\n|\r/);
-			let index = getRandomInt(0, items.length-1)
-			let item = items[index]
-			setSplash(item)
-  		});
-	}
-	
-	// from https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-	function getRandomInt(min, max) {
-		min = Math.ceil(min)
-		max = Math.floor(max)
-		return (Math.floor(Math.random() * (max - min + 1)) + min)
-	}
-	
-	const [splash, setSplash] = useState('');
-
-	/* eslint-disable react-hooks/exhaustive-deps */
-	useEffect(() => {
-
-		// load a random splash
-		loadSplash();
-
-		// setting the background to have no scrolling
-		document.body.style.overflowY = 'hidden'
-		document.body.style.overflowX = 'hidden'
-
-		// return is fired on componentwillunmount
-		return() =>{
-			document.body.style.overflowY = 'scroll'
-			document.body.style.overflowX = 'scroll'
-		}
-
-	}, []);
-	
-
 	return (
 		<div>
 			<div id='homepageTitle'>
-				<p className="title">Hey, it's <br/><span>Owen Moogk</span></p>
-				<p id="splash" onClick={() => loadSplash()}>
-					<svg focusable="false" width="20px" height="30px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><path d="M21 12l-7-9v4.99L3 8v8h11v5l7-9z" stroke="var(--pink)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></g></svg>
-					&nbsp;{splash}&nbsp;
-					<svg focusable="false" width="20px" height="30px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" style={{transform: 'rotate(180deg)'}}><g fill="none"><path d="M21 12l-7-9v4.99L3 8v8h11v5l7-9z" stroke="var(--blue)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></g></svg>
-				</p>
+				<span className='larger'>
+					turning ideas into
+					<br />
+					<span className='special'>technology.</span>
+				</span>
 			</div>
 		</div>
 	);
