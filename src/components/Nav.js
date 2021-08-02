3f142e6cfcc71b1dcf40e93fe79ca84b393f886e
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-08-02 09:34:56 -0400

    fragments :bomb:

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,7 +12,7 @@ export default function Nav(props) {
 	})
 
 	return (
-		<div>
+		<>
 			<svg id="navButton" onClick={()=>setShown(!shown)} preserveAspectRatio="xMidYMid meet" viewBox="0 0 48 48"><g fill="none" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"><path d="M7.95 11.95h32"/><path d="M7.95 23.95h32"/><path d="M7.95 35.95h32"/></g></svg>
 			<div id="navBox" style={{display: shown ? "flex" : ''}}>
 				<div className='navTitle'><a href='/'>Owen Moogk</a></div>
@@ -28,6 +28,6 @@ export default function Nav(props) {
 				</div>
 				<Links />
 			</div>
-		</div>
+		</>
 	);
 }
--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,13 +1,11 @@
 export default function Homepage() {
 	return (
-		<div>
-			<div id='homepageTitle'>
-				<span className='larger'>
-					turning ideas into
-					<br />
-					<span className='special'>technology.</span>
-				</span>
-			</div>
+		<div id='homepageTitle'>
+			<span className='larger'>
+				turning ideas into
+				<br />
+				<span className='special'>technology.</span>
+			</span>
 		</div>
 	);
 }
