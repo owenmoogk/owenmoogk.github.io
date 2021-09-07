6ee9afd4f5a67b9de1ef96373f86da3fd8fadb84
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-09-07 14:55:58 -0400

    added transitions to work items :surfer:

--- src/components/work/Workpage.js
+++ src/components/work/Workpage.js
@@ -5,15 +5,26 @@ import WorkItem from './WorkItem'
 export default function Workpage() {
 
 	const [workData, setWorkData] = useState()
-	const [activeDropdown, setActiveDropdown] = useState()
+	const [activeDropdown, setActiveDropdown] = useState(-1)
 
 	function toggleActive(num) {
-		if (num === activeDropdown) {
+		var growDiv = document.getElementById('work'+num);
+		if (growDiv.clientHeight) {
+			growDiv.style.height = 0;
 			setActiveDropdown(-1)
 		}
 		else {
+			var toClose = document.getElementById('work'+activeDropdown)
+			if (toClose){
+				toClose.style.height = 0
+			}
+
+			// enlarge the clicked
+			var wrapper = document.querySelector('#measure'+num);
+			growDiv.style.height = (wrapper.clientHeight + 10) + "px";
 			setActiveDropdown(num)
 		}
+
 	}
 
 	useEffect(() => {
@@ -24,8 +35,8 @@ export default function Workpage() {
 
 	function loadArrow(num) {
 		return (
-			<span class='arrow' style={{ marginRight: '40px' }}>
-				<svg width="20px" height="20px" style={{ transform: activeDropdown === num ? '' : 'rotate(-90deg)' }} fill='var(--textColor)' viewBox="0 0 16 16"><path d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" /></svg>
+			<span className='arrow' style={{ marginRight: '40px' }}>
+				<svg width="20px" height="20px" style={{ transform: activeDropdown === num ? '' : 'rotate(-90deg)', transition: '0.3s'}} fill='var(--textColor)' viewBox="0 0 16 16"><path d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" /></svg>
 			</span>
 		)
 
@@ -39,23 +50,31 @@ export default function Workpage() {
 				? <div id='workItems'>
 
 					<h1 onClick={() => toggleActive(0)}>{loadArrow(0)}Experience</h1>
-					<div style={{ display: activeDropdown === 0 ? '' : 'none', width: '90%'}}>
-						{workData.experience.map((data, key) => <WorkItem key={key} data={data} />)}
+					<div className='workDisplayer' id='work0'>
+						<div id='measure0' class='measureDiv'>
+							{workData.experience.map((data, key) => <WorkItem key={key} data={data} />)}
+						</div>
 					</div>
 
 					<h1 onClick={() => toggleActive(1)}>{loadArrow(1)}Education</h1>
-					<div style={{ display: activeDropdown === 1 ? '' : 'none' }}>
-						{workData.education.map((data, key) => <WorkItem key={key} data={data} />)}
+					<div className='workDisplayer' id='work1'>
+						<div id='measure1' class='measureDiv'>
+							{workData.education.map((data, key) => <WorkItem key={key} data={data} />)}
+						</div>
 					</div>
 
 					<h1 onClick={() => toggleActive(2)}>{loadArrow(2)}Volunteer Experience</h1>
-					<div style={{ display: activeDropdown === 2 ? '' : 'none' }}>
-						{workData.volunteer.map((data, key) => <WorkItem key={key} data={data} />)}
+					<div className='workDisplayer' id='work2'>
+						<div id='measure2' class='measureDiv'>
+							{workData.volunteer.map((data, key) => <WorkItem key={key} data={data} />)}
+						</div>
 					</div>
 
 					<h1 onClick={() => toggleActive(3)}>{loadArrow(3)}Certifications / Courses</h1>
-					<div style={{ display: activeDropdown === 3 ? '' : 'none' }}>
-						{workData.certifications.map((data, key) => <WorkItem key={key} data={data} />)}
+					<div className='workDisplayer' id='work3'>
+						<div id='measure3' class='measureDiv'>
+							{workData.certifications.map((data, key) => <WorkItem key={key} data={data} />)}
+						</div>
 					</div>
 				</div>
 				: null
