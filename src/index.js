652ae461437abd048289d87008d13d91f8399433
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-11-08 13:13:59 -0500

    realized i actually don't need to render them in index.js :card_index:

--- src/index.js
+++ src/index.js
@@ -2,26 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-if (window.location.pathname === '/v1') {
-	fetch('./old-versions/index.html')
-		.then(function (response) {
-			return response.text();
-		})
-		.then(function (body) {
-			console.log(body);
-		});
-}
-else if (window.location.pathname === '/v2') {
-	ReactDOM.render(
-		<h1>testing testing</h1>,
-		document.getElementById('root')
-	)
-}
-else {
-	ReactDOM.render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-		document.getElementById('root')
-	);
-}
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
