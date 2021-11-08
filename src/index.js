ff5e5a5ea9c195e74c81bbe13776641892a25f1b
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-11-08 12:44:26 -0500

    added v1 :100:

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+if (window.location.pathname === '/v1') {
+	fetch('./old-versions/index.html')
+		.then(function (response) {
+			return response.text();
+		})
+		.then(function (body) {
+			console.log(body);
+		});
+}
+else {
+	ReactDOM.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+		document.getElementById('root')
+	);
+}
