489e97266f40abf1b6d5cbe55b3c181b4616eb38
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-12-05 22:01:36 -0500

    overhauled home page

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import SkillIcon from "./SkillIcon";
+import FeaturedIcon from "components/projects/FeaturedIcon";
 
 export default function Homepage() {
 
 	const [skillsLoaded, setSkillsLoaded] = useState(false)
 
+	function isElementInViewport (el) {
+		var rect = el.getBoundingClientRect();
+		return(
+			rect.top-window.innerHeight < 0
+		)
+	}
+	
 	function animateSkills() {
-		if (window.scrollY >= window.innerHeight/2) {
+		var el = document.getElementById('skillsTable')
+		if (isElementInViewport(el)) {
 			setSkillsLoaded(true)
 		}
-		else if (window.scrollY < 100) {
+		else {
 			setSkillsLoaded(false)
 		}
 	}
@@ -21,61 +30,65 @@ export default function Homepage() {
 	return (
 		<div id='homePage'>
 			<div id='homepageTitle'>
-				<div className='larger'>
-					turning ideas into
-					<br />
-					<span className='special'>technology.</span>
+				<div id='text'>
+					<div className='larger'>
+						Hey, I'm
+						<br />
+						<span className='special'>Owen Moogk</span>
+					</div>
+					<p className='subtitle'>Aspiring Software Developer</p>
 				</div>
-
-				<svg onClick={() => {
-					window.scroll(0, window.innerHeight - 70)
-				}} id='downArrow' width="30px" height="30px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><path d="M4 9l8 8l8-8" stroke="var(--textColor)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" /></g></svg>
-
-			</div>
-
-			<div id='about'>
-				<div id='homepageSplitscreen'>
-					<div>
-
-						<h1>About</h1>
-						<p>
-							Hey there, it's Owen! This is my website I made using ReactJS to work on my web development skills as well as showcase my work and talents. If you have any inquires, questions, or comments, feel free to <a href='/contact'>reach out</a>!
-
-							<br /><br />
-
-							I am currently a highschool student in Grade 12 at <a href="https://bci.wrdsb.ca" target="_blank" rel='noreferrer'>Bluevale Collegiate Institute</a>,
-							who is quite passionate about engineering and design. Check out my <a href="projects">projects</a> page for details on personal endeavors with coding, hardware, web development, and 3D printing.
-							
-							<br/><br/>
-
-							To pursue more learning and experience in these fields I have participated in many extracurriculars, including working as a student on the FRC robotics team <a href="https://2702rebels.com" target="_blank" rel='noreferrer'>2702 Rebels</a>, involving myself in the <a href="https://dukeofed.org" target="_blank" rel='noreferrer'>Duke of Edinburgh's Award</a>, and attending the prestigious month-long <a href="https://shad.ca" target="_blank" rel='noreferrer'>SHAD</a> program. More info about all of this can be found on my <a href='work'>work</a> page.
-						</p>
-					</div>
-
-					<div id='skills'>
-						<h1>Skills</h1>
-						<table id='skillsTable'>
-							<h3>Web Development</h3>
-							<SkillIcon name='ReactJS' percent={80} doLoad={skillsLoaded} />
-							<SkillIcon name='Javascript' percent={90} doLoad={skillsLoaded} />
-							<SkillIcon name='HTML / DOM' percent={90} doLoad={skillsLoaded} />
-							<SkillIcon name='CSS' percent={75} doLoad={skillsLoaded} />
-
-							<h3>Coding</h3>
-							<SkillIcon name='Python' percent={90} doLoad={skillsLoaded} />
-							<SkillIcon name='Django' percent={40} doLoad={skillsLoaded} />
-							<SkillIcon name='C++ / Arduino' percent={30} doLoad={skillsLoaded} />
-							<SkillIcon name='Git' percent={50} doLoad={skillsLoaded} />
-
-							<h3>Mechanical</h3>
-							<SkillIcon name='Solidworks' percent={60} doLoad={skillsLoaded} />
-							<SkillIcon name='3D Printing' percent={70} doLoad={skillsLoaded} />
-						</table>
-					</div>
-
+				<div className='photo'>
+					<img src='/assets/pfp-bw.png' />
 				</div>
 			</div>
+			<div>
+				<p>
+					Hey there, it's Owen! This is my website I made using ReactJS to work on my web development skills as well as showcase my work and talents. If you have any inquires or comments, feel free to <a href='/contact'>reach out</a>! PS. I've also added some hidden features around the site, so have a look around!
+				</p>
+			</div>
+			<div>
+				<h1>Featured Projects</h1>
 
+				<div id='featuredProjectContainer'>
+					<FeaturedIcon title={"2702 Rebels 2020 Robot"} name={"2702-2020"} type={''} link={''} />
+					<FeaturedIcon title={"Pathfinding Visualizer"} name={"pathfinding-visualizer"} type={''} link={''} />
+				</div>
+			</div>
+			<div>
+
+				<p>
+					<i>Wait, you want to know about me?</i> In that case, I am currently a highschool student in Grade 12 at <a href="https://bci.wrdsb.ca" target="_blank" rel='noreferrer'>Bluevale Collegiate Institute</a>,
+					who is quite passionate about engineering and design. Check out my <a href="projects">projects</a> page for details on personal endeavors with coding, hardware, web development, and 3D printing.
+				</p>
+			</div>
+			<div>
+				<p>
+
+					To pursue more learning and experience in these fields I have participated in many extracurriculars, including working as a student on the FRC robotics team <a href="https://2702rebels.com" target="_blank" rel='noreferrer'>2702 Rebels</a>, involving myself in the <a href="https://dukeofed.org" target="_blank" rel='noreferrer'>Duke of Edinburgh's Award</a>, and attending the prestigious month-long <a href="https://shad.ca" target="_blank" rel='noreferrer'>SHAD</a> program. More info about all of this can be found on my <a href='work'>work</a> page.
+				</p>
+			</div>
+
+			<div id='skills'>
+				<p>Lastly, these are some skills I've been working to refine over the last few years. To see them in action check out my <a href='https://github.com/owenmoogk' target='_blank' rel='noreferrer'>Github</a>.</p>
+				<table id='skillsTable'>
+					<h3>Web Development</h3>
+					<SkillIcon name='ReactJS' percent={80} doLoad={skillsLoaded}/>
+					<SkillIcon name='Javascript' percent={90} doLoad={skillsLoaded}/>
+					<SkillIcon name='HTML / DOM' percent={90} doLoad={skillsLoaded}/>
+					<SkillIcon name='CSS' percent={75} doLoad={skillsLoaded}/>
+
+					<h3>Coding</h3>
+					<SkillIcon name='Python' percent={90} doLoad={skillsLoaded}/>
+					<SkillIcon name='Django' percent={40} doLoad={skillsLoaded}/>
+					<SkillIcon name='C++ / Arduino' percent={30} doLoad={skillsLoaded}/>
+					<SkillIcon name='Git' percent={50} doLoad={skillsLoaded}/>
+
+					<h3>Mechanical</h3>
+					<SkillIcon name='Solidworks' percent={60} doLoad={skillsLoaded}/>
+					<SkillIcon name='3D Printing' percent={70} doLoad={skillsLoaded}/>
+				</table>
+			</div>
 		</div>
 	);
 }
