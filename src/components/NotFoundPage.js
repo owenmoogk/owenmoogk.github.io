702ecad20c7b0d80760cd292c185d3c0a5f0e486
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-12-05 19:51:01 -0500

    updated not found page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,10 +1,23 @@
-export default function NotFoundPage(props){
-    return(
+import { useState } from 'react'
+import Background from './background/Background.js'
+
+export default function NotFoundPage() {
+    const [party, setParty] = useState(false)
+
+    function startTheParty(){
+        setParty(true)
+    }
+
+    return (
         <div className='main'>
             <h1 className='title'>404!</h1>
             <p className='subtitle'>Uh oh, looks like I lost you.</p>
             <p><a href='/'>Take me home</a></p>
-            <p>or... we can <a href='https://owenmoogk.tk/v6/'>party</a>!</p>
+            <p>or... we can <a onClick={() => startTheParty()}>party</a>!</p>
+            {party
+                ? <Background />
+                : null
+            }
         </div>
     )
 }
