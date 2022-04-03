ece56c04db88d481e2226f0a040f0edeaef32421
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2022-04-03 11:56:42 -0400

    cleaned up 404 page not found

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,23 +1,11 @@
-import { useState } from 'react'
-import Background from './background/Background.js'
-
 export default function NotFoundPage() {
-    const [party, setParty] = useState(false)
-
-    function startTheParty(){
-        setParty(true)
-    }
 
     return (
         <div className='main'>
             <h1 className='title'>404!</h1>
             <p className='subtitle'>Uh oh, looks like I lost you.</p>
-            <p><a href='/'>Take me home</a></p>
-            <p>or... we can <a onClick={() => startTheParty()}>party</a>!</p>
-            {party
-                ? <Background />
-                : null
-            }
+            <p>If there should be a valid page here, please <a href='/contact'>let me know</a>.</p>
+            <p>Country roads....<a href='/'>Take me home</a>.</p>
         </div>
     )
 }
