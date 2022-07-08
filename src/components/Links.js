d50b104d785629c9f28caa3f06a7c1bab8b2db0b
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2022-07-08 16:06:18 -0400

    removed twitter link

--- src/components/Links.js
+++ src/components/Links.js
@@ -17,12 +17,12 @@ export default function Links() {
 					<line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
 				</svg>
 			</a>
-			<a href="https://twitter.com/owen_moogk" aria-label="Twitter" target="_blank" rel="noreferrer">
+			{/* <a href="https://twitter.com/owen_moogk" aria-label="Twitter" target="_blank" rel="noreferrer">
 				<svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="lightblue" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-twitter">
 					<title>Twitter</title>
 					<path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
 				</svg>
-			</a>
+			</a> */}
 			<a href="https://www.linkedin.com/in/owen-moogk-1ab9371b8/" aria-label="Linkedin" target="_blank" rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="lightblue" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-linkedin">
 				<title>LinkedIn</title>
 				<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
