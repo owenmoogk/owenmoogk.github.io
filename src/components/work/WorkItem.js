3b4a01f1ec2cd0602166e1f9bea1569c3aa48e76
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2022-07-19 21:55:19 -0400

    fixed some spacing and format issues on work page

--- src/components/work/WorkItem.js
+++ src/components/work/WorkItem.js
@@ -8,35 +8,33 @@ export default function WorkItem(props) {
 	return (
 		<div className="workItem">
 
-			<div style={{ display: "flex" }}>
-				<div className='graphics'>
-					<div className='svg'><div className="innerSvg"></div></div>
-					<div className='line'></div>
-				</div>
+			<div className='graphics'>
+				<div className='svg'><div className="innerSvg"></div></div>
+				<div className='line'></div>
+			</div>
 
-				<div className='text'>
-					<div className="titleBlock" onClick={() => setCollapsed(!collapsed)}>
-						<div className='workTitle'>{data.title}</div>
-						{data.subtitle
-							? <div>{data.subtitle} • {data.date}</div>
-							: <div>{data.date}</div>
-						}
-					</div>
-					{data.description
-						? <div className='content' style={{height: collapsed ? '0' : ''}}>
-							<p dangerouslySetInnerHTML={{ __html: data.description }} />
-							<div>
-								{data.ul
-									? <ul>
-										{data.ul.map((li, key) => <li key={key} dangerouslySetInnerHTML={{ __html: li }}></li>)}
-									</ul>
-									: null
-								}
-							</div>
-						</div>
-						: null
+			<div className='text'>
+				<div className="titleBlock" onClick={() => setCollapsed(!collapsed)}>
+					<div className='workTitle'>{data.title}</div>
+					{data.subtitle
+						? <div>{data.subtitle} • {data.date}</div>
+						: <div>{data.date}</div>
 					}
 				</div>
+				{data.description
+					? <div className='content' style={{ height: collapsed ? '0' : '' }}>
+						<p dangerouslySetInnerHTML={{ __html: data.description }} />
+						<div>
+							{data.ul
+								? <ul>
+									{data.ul.map((li, key) => <li key={key} dangerouslySetInnerHTML={{ __html: li }}></li>)}
+								</ul>
+								: null
+							}
+						</div>
+					</div>
+					: null
+				}
 			</div>
 		</div>
 	);
