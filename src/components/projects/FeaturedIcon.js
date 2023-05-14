7e6b51f4b2fe0ae478d7f95ce87861e6f4695612
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2023-05-13 23:53:08 -0400

    changed project borders

--- src/components/projects/FeaturedIcon.js
+++ src/components/projects/FeaturedIcon.js
@@ -6,23 +6,25 @@ export default function FeaturedIcon(props) {
 	var types = props.type.toLowerCase().split(' ')
 
 	// ok so c++ gives an error when put into a variable, so we just change it for the filtering stuff, display still says C++
-	for (var i = 0; i < types.length; i++){
-		if (types[i] === 'c++'){
-			types[i] = 'c'
-		}
-	}
+	// for (var i = 0; i < types.length; i++){
+	// 	if (types[i] === 'c++'){
+	// 		types[i] = 'c'
+	// 	}
+	// }
 
-	var borderColor;
+	// var borderColor;
 	
-	if (types.length === 1){
-		borderColor = 'linear-gradient(white, white), linear-gradient(45deg, var(--'+types[0]+', gray), var(--'+types[0]+', gray))'
-	}
-	else if (types.length === 2){
-		borderColor = 'linear-gradient(white, white), linear-gradient(45deg, var(--'+types[0]+', gray) 40%, var(--'+types[1]+', gray) 60%)'
-	}
-	else if (types.length === 3){
-		borderColor = 'linear-gradient(white, white), linear-gradient(45deg, var(--'+types[0]+', gray) 30%, var(--'+types[1]+', gray) 40%, var(--'+types[1]+', gray) 60%, var(--'+types[2]+', gray) 70%)'
-	}
+	// if (types.length === 1){
+	// 	borderColor = 'linear-gradient(white, white), linear-gradient(45deg, var(--'+types[0]+', gray), var(--'+types[0]+', gray))'
+	// }
+	// else if (types.length === 2){
+	// 	borderColor = 'linear-gradient(white, white), linear-gradient(45deg, var(--'+types[0]+', gray) 40%, var(--'+types[1]+', gray) 60%)'
+	// }
+	// else if (types.length === 3){
+	// 	borderColor = 'linear-gradient(white, white), linear-gradient(45deg, var(--'+types[0]+', gray) 30%, var(--'+types[1]+', gray) 40%, var(--'+types[1]+', gray) 60%, var(--'+types[2]+', gray) 70%)'
+	// }
+
+	var borderColor = 'linear-gradient(white, white), linear-gradient(var(--textColor), var(--textColor))'
 	
 	return (
 		<a href={link} style={{margin: '20px'}} className='featuredIconLink'>
