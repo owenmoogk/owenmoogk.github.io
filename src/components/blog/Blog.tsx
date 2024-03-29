52a040d924eeb7b6fed2595adcbb34e22dbfc1c1
Owen Moogk <owenmoogk@gmail.com>
2024-03-29 16:40:12 -0600

    improve the display of the blog
    add hr between blogs, and split with (... continue to medium) at a space, not just randomly

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -18,20 +18,27 @@ export default function Blog() {
 	const url = "https://medium.com/feed/@owenmoogk";
 	const [blogData, setBlogData] = useState<BlogPost[]>()
 
-	async function getBlogs(){
-			fetch(corsProxy + url)
+	async function getBlogs() {
+		fetch(corsProxy + url)
 			.then((res) => res.text())
 			.then(str => new window.DOMParser().parseFromString(str, "text/xml"))
 			.then(data => {
 				var posts: BlogPost[] = []
-				for (var post of data.getElementsByTagName("item")){
-					console.log((post.getElementsByTagName("content:encoded")[0] as HTMLElement).textContent)
-					var postObject = {
-						title: post.getElementsByTagName("title")[0].textContent,
-						link: post.getElementsByTagName("link")[0].textContent,
-						date: new Date(Date.parse((post.getElementsByTagName("pubDate")[0] as HTMLElement).textContent || "")),
-						content: new window.DOMParser().parseFromString((post.getElementsByTagName("content:encoded")[0] as HTMLElement).textContent?.replace("<br>", " ").replace("</p>", " ") || "", "text/html").body.innerText,
-					} as BlogPost
+				for (var post of data.getElementsByTagName("item")) {
+					var postObject: BlogPost = {
+						title: post.getElementsByTagName("title")[0].textContent ?? "",
+						link: post.getElementsByTagName("link")[0].textContent ?? "",
+						date: new Date(Date.parse((post.getElementsByTagName("pubDate")[0] as HTMLElement).textContent ?? "")),
+						content: new window.DOMParser().parseFromString((post.getElementsByTagName("content:encoded")[0] as HTMLElement).textContent?.replaceAll("<br>", " ").replaceAll("</p><p>", " ") ?? "", "text/html").body.innerText,
+					}
+					// cut off the content at the end of a word, and add ...
+					var cutoffIndex = 300
+					while (postObject.content[cutoffIndex] != " ") {
+						cutoffIndex -= 1
+					}
+					postObject.content = postObject.content.substring(0, cutoffIndex)
+					postObject.content += "..."
+
 					posts.push(postObject)
 				}
 				setBlogData(posts)
@@ -49,34 +56,37 @@ export default function Blog() {
 			</Helmet>
 			<p className="title">Blog</p>
 			<div id='blogPage'>
-			{
-				blogData?.map((post, key) => 
-					<BlogItem post = {post} key = {key}/>
-				)
-			}
+				{
+					blogData?.map((post, key) =>
+						<>
+							<BlogItem post={post} key={key} />
+							<hr />
+						</>
+					)
+				}
 			</div>
 		</div>
 	);
 }
 
-function BlogItem(props:{
+function BlogItem(props: {
 	post: BlogPost
 }) {
 
 	function formatDate(date: Date) {
-    // Define months array for formatting
-    var months = ["January", "February", "March", "April", "May", "June", "July",
-                  "August", "September", "October", "November", "December"];
+		// Define months array for formatting
+		var months = ["January", "February", "March", "April", "May", "June", "July",
+			"August", "September", "October", "November", "December"];
 
-    // Extract date components
-    var day = date.getDate();
-    var month = months[date.getMonth()];
-    var year = date.getFullYear();
+		// Extract date components
+		var day = date.getDate();
+		var month = months[date.getMonth()];
+		var year = date.getFullYear();
 
-    // Format the date string
-    var formattedDate = month + " " + day + ", " + year;
+		// Format the date string
+		var formattedDate = month + " " + day + ", " + year;
 
-    return formattedDate;
+		return formattedDate;
 	}
 
 	var post = props.post
@@ -84,7 +94,7 @@ function BlogItem(props:{
 		<div className="blogPost">
 			<h3><a href={post.link} className="postTitle" target='_blank' rel='noreferrer'>{post.title}</a></h3>
 			<p className="subtitle">{formatDate(post.date)}</p>
-			<p className="blogContent">{post.content.substring(0, 350)}...  &nbsp; <a href={post.link} target="_blank" rel='noreferrer'>continue on Medium</a></p>
+			<p className="blogContent">{post.content} &nbsp; <a href={post.link} target="_blank" rel='noreferrer'>continue on Medium</a></p>
 		</div>
 	)
 }
