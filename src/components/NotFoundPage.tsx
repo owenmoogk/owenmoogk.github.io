02f2240f649f8817c5d079b21234f0dae320da7f
Owen Moogk <owenmoogk@gmail.com>
2024-03-29 16:55:48 -0600

    improve the styling on the 404 page

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,11 +1,14 @@
 export default function NotFoundPage() {
 
     return (
-        <div className='main'>
+        <div className='main' id="notFoundPage">
             <h1 className='title'>404!</h1>
             <p className='subtitle'>Uh oh, looks like I lost you.</p>
-            <p>If there should be a valid page here, please <a href='/contact'>let me know</a>.</p>
-            <p>Country roads....<a href='/'>Take me home</a>.</p>
+            <p>
+				If there should be a valid page here, please <a href='/contact'>let me know</a>.
+				<br/><br/>
+				Country roads... <a href='/'>take me home</a>.
+			</p>
         </div>
     )
 }
