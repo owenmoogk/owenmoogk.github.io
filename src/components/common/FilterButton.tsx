e42f3393cce522d3f038e3140e1aa7aaf9681a36
Owen Moogk <owenmoogk@gmail.com>
2024-08-08 15:34:57 -0400

    fixed a filter button bug

--- src/components/common/FilterButton.tsx
+++ src/components/common/FilterButton.tsx
@@ -1,14 +1,16 @@
 import { Dispatch, SetStateAction } from "react";
 
 export default function FilterButton(props: {
+	displayName?: string;
 	name: string;
 	setFilter: Dispatch<SetStateAction<string>>;
 	filter: string;
 	handle?: string;
 }) {
-	var nameLower = props.name.toLowerCase()
+	var nameLower = props.name.toLowerCase().replace(" ", "_")
 	var handle = props.handle ?? nameLower
 	var filter = props.filter.toLowerCase()
+	var displayName = props.displayName ?? props.name
 
 	return (
 		<span className={"sort_" + nameLower + " btn " + (filter === handle ? 'active' : '')} onClick={() => props.setFilter(handle)} style={{
@@ -16,7 +18,7 @@ export default function FilterButton(props: {
 			color: 'var(--' + nameLower + ')',
 		}}
 		>
-			<p>{props.name}</p>
+			<p>{displayName}</p>
 			<style dangerouslySetInnerHTML={{
 				__html: `
 				.sort_${nameLower}.active{
@@ -24,7 +26,7 @@ export default function FilterButton(props: {
 					color: var(--textColor) !important
 				}
 				.sort_${nameLower}:hover{
-					background-color: var(--${handle}) !important;
+					background-color: var(--${nameLower}) !important;
 					color: var(--textColor) !important
 				}
 			`}} />
