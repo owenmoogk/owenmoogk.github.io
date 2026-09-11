3e282c972d369a152087a76d7435ea6d6fe4d6f1
Owen Moogk <owenmoogk@gmail.com>
2026-09-11 16:57:39 -0400

    Default to lightmode

--- src/main.tsx
+++ src/main.tsx
@@ -35,7 +35,7 @@ createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <Router>
       <MantineProvider
-        defaultColorScheme="dark"
+        defaultColorScheme="light"
         theme={mantineTheme}
         cssVariablesResolver={mantineCssVariableResolver}
       >
